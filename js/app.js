4f71e141da4ad1386d51522c4e8729b21749c433
simon01092024 <simonkudus510@gmail.com>
2024-02-05 15:03:03 -0800

    pseudo code for js

---
+++ js/app.js
@@ -0,0 +1,59 @@
+/*----- constants -----*/
+
+//dice faces images
+
+/*----- app's state (variables) -----*/
+
+// Human Player Roll first 
+// Player diceRoll
+
+// Computer Roll second 
+// Computer diceRoll
+
+// point
+
+// results 
+
+/*----- cached element references -----*/
+
+// buttons
+// random number generator 
+
+/*----- event listeners -----*/
+
+// diceRoll 
+
+// buttons 
+
+/*----- functions -----*/
+
+// Board
+
+// start
+
+// results 
+
+// play again
+
+// quit
+
+
+// Additional Notes can use info for ReadMe 
+
+// Get 2 separate values of the sum of the dice roll 
+// 	1 for the sum of dice for the player 
+// 	1 for the sum of the for computer  
+
+// A sum function that returns the value 2 numbers generated 1 for each die rolled. 
+
+// Now create a function that checks if the sum total is a winner 
+
+// If player rolls a 7 or 11 you on first roll you win right away so that would be the first thing to check maybe with a if statement 
+// Then check if the player loses on the first roll if the sum total of dice rolled equals 
+// 2(snake eyes) or 12(double sixes)
+
+// After that is checked then create a function makes the winning roll what number you first roll 7 or 11 and keep losing rolls as snake eyes and double sixes
+
+// Now find a way to invoke the functions
+
+// Add event listeners to buttons that say roll dice start game and maybe end game early (same as losing but maybe need to break out of a function.
